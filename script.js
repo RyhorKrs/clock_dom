3c0b7a1275d0beb39fb4164a787243134e464bf8
Ryhor <krasniakovg01@gmail.com>
2021-06-27 13:26:07 +0300

    add main clock container and hooked up script.js file

---
+++ script.js
@@ -0,0 +1,3 @@
+let container = document.createElement('div');
+container.style.cssText = 'width: 600px; height: 600px; background-color: gold; border-radius: 50%; position: relative; margin: 0 auto;';
+document.body.append(container);
