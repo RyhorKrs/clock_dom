4cbdaa37d6a5fd1d3f1274112f07fef4b7b7d4ad
Ryhor <krasniakovg01@gmail.com>
2021-06-27 14:15:29 +0300

    add arrows

--- script.js
+++ script.js
@@ -10,5 +10,21 @@ for (let i = 1; i < 13; i++) {
     let item = document.createElement('div');
     item.textContent = i;
     item.style.cssText = 'width: 70px; height: 70px; background-color: green; position: absolute; top: -35px; left: -35px; font-size: 30px; font-family: monospace; border-radius: 50%; text-align: center; vertical-align: middle; line-height: 70px; transform: translateY(-250px) rotate('+ -360/12*i +'deg);';
-  subcontainer.append(item);
+    subcontainer.append(item);
   };
+
+let arrowSubcontainer = document.createElement('div');
+arrowSubcontainer.style.cssText = 'width: 0px; height: 0px; position: relative; top: 50%; left: 50%;';
+container.append(arrowSubcontainer);
+
+let hArrow = document.createElement('div');
+hArrow.style.cssText = 'width: 10px; height: 130px; background-color: black; opacity: 0.9; border-radius: 10px; position: absolute; bottom: 0; left: -5px; transform-origin: bottom;';
+arrowSubcontainer.append(hArrow);
+
+let minArrow = document.createElement('div');
+minArrow.style.cssText = 'width: 6px; height: 180px; background-color: black; opacity: 0.9; border-radius: 10px; position: absolute; bottom: 0; left: -3px; transform-origin: bottom;';
+arrowSubcontainer.append(minArrow);
+
+let secArrow = document.createElement('div');
+secArrow.style.cssText = 'width: 2px; height: 230px; background-color: black; opacity: 0.9; border-radius: 10px; position: absolute; bottom: 0; left: -1px; transform-origin: bottom;';
+arrowSubcontainer.append(secArrow);
